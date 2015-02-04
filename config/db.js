9c69d3bbf3d446c426c452fa19b8a64c960e8211
sagar singh <ssing128@gmail.com>
2015-02-04 07:00:31 -0800

    set up config for db and routes in /config

---
+++ config/db.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var conn = mongoose.connection; //Default Connection
+
+var uri = 'mongodb://localhost/link-shortner';
+
+conn.on('error', console.error.bind(console, "Mongoose connection error: "));
+conn.once('open', function() {
+  console.log("connected to MongoDb: ", uri);
+});
+
+mongoose.connect(uri);
+
+module.exports = conn;
